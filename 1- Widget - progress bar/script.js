47b9a73343239cb057ca9b43df36a7683a858f91
Maud-Cht <123174319+Maud-Cht@users.noreply.github.com>
2025-11-11 10:55:24 +0100

    Delete 1- Widget - progress bar directory

--- 1- Widget - progress bar/script.js
+++
@@ -1,9 +0,0 @@
-const progressBar = document.getElementById("progress-bar");
-const progressText = document.getElementById("progress-text");
-const progressInput = document.getElementById("progress-input");
-
-progressInput.addEventListener("input", () => {
-  const value = progressInput.value;
-  progressBar.style.width = value + "%";
-  progressText.textContent = value + "%";
-});
